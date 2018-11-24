1f21a83a17cdbf053df2888096e49f9ed1d8f87d
Lior Bar-On <baronlior@gmail.com>
2018-11-24 15:50:24 +0200

    add setup

--- src/main/kotlin/com/ni/ObjectMapperBenchmark.kt
+++ src/main/kotlin/com/ni/ObjectMapperBenchmark.kt
@@ -1,5 +1,6 @@
 package com.ni
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.ni.json.improvedJsonMapper
 import org.openjdk.jmh.annotations.*
 import org.openjdk.jmh.infra.Blackhole
@@ -11,19 +12,26 @@ import java.util.concurrent.TimeUnit
 @CompilerControl(CompilerControl.Mode.DONT_INLINE)
 open class ObjectMapperBenchmark {
 
-  @State(Scope.Thread)
+  @State(Scope.Benchmark)
   open class MyState {
-    private val raw = listOf("avc230920349", "some_data_to_serialize",
-        mapOf("a" to 1, "B" to 2.0, "not" to "too little")
-    )
-    val mapper = improvedJsonMapper()
+
+    lateinit var data : Any
+    lateinit var mapper: ObjectMapper
 
     @Param("tiny" , "small", "large") var index : String = ""
-    val data = mapOf(
-        "tiny" to raw[0]!!,
-        "small" to raw,
-        "large" to (1..1_000).map { it to raw }
-    )
+
+    @Setup(Level.Trial)
+    fun setup() {
+      val raw = listOf("avc230920349", "some_data_to_serialize",
+          mapOf("a" to 1, "B" to 2.0, "not" to "too little")
+      )
+      this.data = mapOf(
+          "tiny" to raw[0]!!,
+          "small" to raw,
+          "large" to (1..1_000).map { it to raw }
+      )[index]!!
+      this.mapper = improvedJsonMapper()
+    }
 
     @TearDown(Level.Trial)
     fun doTearDown() {
@@ -34,13 +42,13 @@ open class ObjectMapperBenchmark {
   @Benchmark
   fun newMapper(bh: Blackhole, state: MyState) {
     val mapper = improvedJsonMapper()
-    val json = mapper.writeValueAsString(state.data[state.index])
+    val json = mapper.writeValueAsString(state.data)
     bh.consume(json)
   }
 
   @Benchmark
   fun reuseMapper(bh: Blackhole, state: MyState) {
-    val json = state.mapper.writeValueAsString(state.data[state.index])
+    val json = state.mapper.writeValueAsString(state.data)
     bh.consume(json)
   }
 
